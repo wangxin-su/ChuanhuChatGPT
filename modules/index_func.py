efaab34e04cd29691e7ab06014bf174d7e56546a
xinwang <azure@sandstar-west-ai-serv.ivwoldvg4s4utbwpcpyt1wkzhe.dx.internal.cloudapp.net>
2024-02-05 13:20:37 +0000

    up

--- modules/index_func.py
+++ modules/index_func.py
@@ -110,13 +110,22 @@ def construct_index(
         embeddings = HuggingFaceEmbeddings(
             model_name="sentence-transformers/distiluse-base-multilingual-cased-v2")
     else:
-        from langchain.embeddings import OpenAIEmbeddings
+        #from langchain.embeddings import OpenAIEmbeddings
+        #if os.environ.get("OPENAI_API_TYPE", "openai") == "openai":
+        #    embeddings = OpenAIEmbeddings(openai_api_base=os.environ.get(
+        #        "OPENAI_API_BASE", None), openai_api_key=os.environ.get("OPENAI_EMBEDDING_API_KEY", api_key))
+        #else:
+        #    embeddings = OpenAIEmbeddings(deployment=os.environ["AZURE_EMBEDDING_DEPLOYMENT_NAME"], openai_api_key=os.environ["AZURE_OPENAI_API_KEY"],
+        #                                  model=os.environ["AZURE_EMBEDDING_MODEL_NAME"], openai_api_base=os.environ["AZURE_OPENAI_API_BASE_URL"], openai_api_type="azure")
+        from langchain.embeddings import AzureOpenAIEmbeddings, OpenAIEmbeddings
         if os.environ.get("OPENAI_API_TYPE", "openai") == "openai":
+            print("BBB")
             embeddings = OpenAIEmbeddings(openai_api_base=os.environ.get(
                 "OPENAI_API_BASE", None), openai_api_key=os.environ.get("OPENAI_EMBEDDING_API_KEY", api_key))
         else:
-            embeddings = OpenAIEmbeddings(deployment=os.environ["AZURE_EMBEDDING_DEPLOYMENT_NAME"], openai_api_key=os.environ["AZURE_OPENAI_API_KEY"],
-                                          model=os.environ["AZURE_EMBEDDING_MODEL_NAME"], openai_api_base=os.environ["AZURE_OPENAI_API_BASE_URL"], openai_api_type="azure")
+            print("AAA")
+            embeddings = AzureOpenAIEmbeddings(deployment=os.environ["AZURE_EMBEDDING_DEPLOYMENT_NAME"], openai_api_key=os.environ["AZURE_OPENAI_API_KEY"],
+                                          model=os.environ["AZURE_EMBEDDING_MODEL_NAME"], azure_endpoint=os.environ["AZURE_OPENAI_API_BASE_URL"], openai_api_type="azure")                                          
     if os.path.exists(index_path) and load_from_cache_if_possible:
         logging.info("找到了缓存的索引文件，加载中……")
         return FAISS.load_local(index_path, embeddings)
